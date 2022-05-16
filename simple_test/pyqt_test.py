cb2dc8afb6e721282558d9a03a0fa835fae787cc
leo.wu <leowu102000@gmail.com>
2022-05-16 23:32:22 +0800

    first comit

---
+++ simple_test/pyqt_test.py
@@ -0,0 +1,35 @@
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+import sys
+from mainwindow import Ui_Dialog
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        self.ui = Ui_Dialog()
+        self.ui.setupUi(self)
+        self.ui_connection()
+        
+    def ui_connection(self):
+        self.setIDButton = self.ui.setIDButton
+        self.setIDButton.clicked.connect(self.buttonClick)
+        
+    def buttonClick(self):
+        IDlineEditText = self.ui.IDlineEdit.text()
+        if(not IDlineEditText.isnumeric()):
+            message_box = QMessageBox()
+            message_box.setWindowTitle ("error")
+            message_box.setInformativeText("please enter a interger number")
+            message_box.exec_()
+        else:
+            self.ui.IDlabel.setText(IDlineEditText)
+    
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+
+    window = MainWindow()
+    window.show()
+
+    sys.exit(app.exec_())
