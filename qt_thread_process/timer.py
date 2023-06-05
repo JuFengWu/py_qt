fb7a42ac50fc0f5097f7309caf0b4747e3a422f5
leo <leowu102000@gmail.com>
2023-06-05 13:51:18 +0800

    modify qt timmer

--- qt_thread_process/timer.py
+++ qt_thread_process/timer.py
@@ -8,12 +8,13 @@ from time import sleep
 import threading
 
 number = 0
-
+global timer
 def count_sheep():
+    print("do timer")
     global number
     number += 1
     process_show(number)
-    if(number):
+    if(number==5):
         timer.stop()
         number = 0
  
@@ -21,15 +22,21 @@ def process_show(currentNumber):
     ui.progressBar.setValue(currentNumber*20) 
 
 def click_button():
-    timer.start(500)
-    ui.label_2.setText("start running")        
+    timer.start(1000)
+    ui.label_2.setText("start running")
+    
+def click_button2():
+    message_box = QMessageBox()
+    message_box.setWindowTitle ("error")
+    message_box.setInformativeText("hello")
+    message_box.exec_()
         
 app = QApplication(sys.argv)
 widget = QWidget()
 ui = Ui_Dialog()
 ui.setupUi(widget)
 ui.pushButton.clicked.connect(click_button)
-
+ui.pushButton_2.clicked.connect(click_button2)
 
 timer = QTimer()
 timer.timeout.connect(count_sheep)
