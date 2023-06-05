c2b5e3925fd11a716ac471d3a50b375d7663a9be
leo <leowu102000@gmail.com>
2023-06-05 16:32:53 +0800

    modify two ui

--- change_ui/show_two_ui.py
+++ change_ui/show_two_ui.py
@@ -3,24 +3,32 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 import sys
-from mainwindow_1 import Ui_Dialog_1
-from mainwindow_2 import Ui_Dialog_2
+from ui1 import Ui_FirstUI
+from ui2 import Ui_ui2
 import time
 
 
 def buttonClick():
     widget2.show()
+    #widget.close()
+
+def clickHello():
+    print("clickHello")
+
+def clickHello2():
+    print("clickHello2")
 
 app = QApplication(sys.argv)
 
 widget2 = QWidget() # or whatever your top-level class is
-ui2 = Ui_Dialog_2()
+ui2 = Ui_ui2()
 ui2.setupUi(widget2)
+ui2.pushButton.clicked.connect(clickHello2)
 
 widget = QWidget()
-ui = Ui_Dialog_1()
-ui.setupUi(widget)
-setIDButton = ui.setIDButton
-setIDButton.clicked.connect(buttonClick)
+ui1 = Ui_FirstUI()
+ui1.setupUi(widget)
+ui1.pushButton.clicked.connect(buttonClick)
+ui1.helloButton.clicked.connect(clickHello)
 widget.show()
 sys.exit(app.exec_())   
